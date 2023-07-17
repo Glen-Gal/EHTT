1115c2a370e33ae7dd2f7d1a009a92d48084a879
Glen-Gal <Jamphelyigzins15@gmail.com>
2023-07-17 21:45:34 +0600

    lightbox patch

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,3 +145,20 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+/* glightbox
+ */
+var glightbox = GLightbox({
+  loop: true,
+  selector: ".glightbox",
+  openEffect: "zoom",
+  closeEffect: "fade",
+  startAt: 0,
+  closeOnOutsideClick: false,
+  zoomable: true,
+  height: "auto",
+  width: "100vw",
+  height: "100vh",
+});
+
+feather.replace();
